e400f0867a6cddc0e90c3776114b0fb01a43b792
07prajwal2000 <manuaradhya07@gmail.com>
2023-04-14 23:39:05 +0530

    added 3 DP problems

---
+++ DP/LongestCommonSubsequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (s1, s2) {
+	let dp = Array(s1.length);
+	for (let i = 0; i < s1.length; i++) {
+		dp[i] = Array(s2.length).fill(-1);
+	}
+	return f(s1, s2, s1.length - 1, s2.length - 1, dp);
+};
+
+var f = (s1, s2, i, j, dp) => {
+	if (i < 0 || j < 0) {
+		return 0;
+	}
+	if (dp[i][j] != -1) return dp[i][j];
+	if (s1[i] == s2[j]) {
+		return 1 + f(s1, s2, i - 1, j - 1, dp);
+	}
+	let l = f(s1, s2, i - 1, j, dp);
+	let r = f(s1, s2, i, j - 1, dp);
+	dp[i][j] = Math.max(l, r);
+	return Math.max(l, r);
+};
