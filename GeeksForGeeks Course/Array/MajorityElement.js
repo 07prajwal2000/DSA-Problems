408b174f6a94b79c8caf1ccd283af88bc9692e65
aradhyap <manuaradhya07@gmail.com>
2023-04-28 18:25:58 +0530

    1 problem added

---
+++ GeeksForGeeks Course/Array/MajorityElement.js
@@ -0,0 +1,38 @@
+var majority = (arr) => {
+  const n = arr.length;
+  let count = 1, res = 0;
+  for (let i = 1; i < n; i++) {
+    if (arr[res] == arr[i]) {
+      count++;
+    } else {
+      count--;
+    }
+    if (count <= 0) {
+      count = 1;
+      res = i;
+    }
+  }
+  // validating if the current element is majority
+  count = 0;
+  for (let i = 0; i < n; i++) {
+    if (arr[i] == arr[res]) {
+      count++;
+    }
+  }
+  return count > (n >> 1) ? arr[res] : -1;
+  
+  // O(n2)
+  for (let i = 0; i < n; i++) {
+    let count = 1;
+    for (let j = 0; j < n; j++) {
+      if (arr[i] != arr[j]) continue;
+      count++;
+    }
+    if (count > (n >> 1)) {
+      return arr[i];
+    }
+  }
+  return -1;
+};
+
+console.log(majority([8, 7, 6, 8, 6, 6, 6, 6, 6]));
