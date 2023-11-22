2f001a59632a05c132a31952278f2df8663d2d20
aradhyap <manuaradhya07@gmail.com>
2023-11-22 11:05:29 +0530

    added kokoEatsBanana problem

---
+++ Problems/Arrays & Strings/kokoEatsBananas.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function (piles, h) {
+	let high = piles.reduce((x, max) => Math.max(x, max));
+	let low = 1;
+
+	while (low <= high) {
+		let mid = (low + high) >> 1;
+		let hrs = getTotalHours(piles, mid);
+		if (hrs <= h) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
+	return low;
+};
+
+function getTotalHours(piles, mid) {
+	let hrs = 0;
+	for (let p of piles) {
+		hrs += Math.ceil(p / mid);
+	}
+	return hrs;
+}
